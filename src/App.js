5dea5c71edfc8c1350d66aea08aa22b1f76cec5f
Lennert Holvoet <lennertholvoet@gmail.com>
2022-11-04 16:58:37 +0100

    First Commit
    Base application - Work In Progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,161 @@
-import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import React, {useState, useEffect } from 'react'
+import {
+  Button,
+  Container,
+  Header,
+  Image,
+  Menu,
+  Segment,
+  Input ,
+  Dropdown
+} from 'semantic-ui-react'
+import useSound from 'use-sound';
+import thumb from "./images/thumb.png"
+import { wordList } from './data/wordlist.js'
+import { useSpeechSynthesis } from 'react-speech-kit';
+import hooray from "./sounds/hooray.wav"
+import error from "./sounds/error.wav"
 
-function App() {
+const App = () => {
+  const [word,setWord] = useState('')
+  const [cap,setCap] = useState(false)
+  const [capLabel,setCapLabel] = useState()
+  const [typed,setTyped] = useState('')
+  const [disabled,setDisabled] = useState(false)
+  const [correct,setCorrect] = useState('olive')
+  const [voiceIndex, setVoiceIndex] = useState(null)
+  const { speak , voices } = useSpeechSynthesis()
+  const voice = voices[voiceIndex] || null
+
+  const [useDutchVoices,setDutchVoices] = useState([])
+
+  useEffect(() => {
+    let arr = []
+    let firstDutch = ''
+    for(let i = 0 ; i < voices.length ; i++)
+      if(voices[i].lang.substring(0,2) === 'nl') {
+        arr[i] = (voices[i])
+        if(firstDutch === '') { setVoiceIndex(i) }  
+      }
+      setDutchVoices(arr)
+  },[voices])
+
+  const chooseWord = () => {
+    setCorrect('olive')
+    setTyped('')
+    let max = wordList.length
+    let rand = Math.floor(Math.random() * max); 
+    let word = wordList[rand]
+    speak({ text : word  , voice })
+    setWord(word)
+  }
+  useEffect(() => {
+    if(cap) {
+      setCapLabel('abc')
+      setWord(word.toUpperCase())
+    }
+    if(!cap) {
+      setCapLabel('ABC')
+      setWord(word.toLowerCase())
+    }
+  },[cap,word])
+
+  const [playError] = useSound(error)
+  const [playHooray] = useSound(hooray)
+
+  const checkTyped = (typedInput) => {
+    setDisabled(true)
+    if(typedInput !== '') {  
+      let typedCompare = typedInput.toLowerCase()
+      let wordCompare = word.toLowerCase()
+      console.log(typedCompare)
+      let numTyped = typedCompare.length
+      console.log(typedCompare.charAt(numTyped - 1))
+      speak({ text : typedCompare.charAt(numTyped - 1) , voice })
+      if(typedCompare.charAt(numTyped - 1) !== wordCompare.charAt(numTyped - 1)) {
+        setTyped(typedCompare.slice(0,-1))
+        setCorrect('red')
+        setTimeout(() => {
+          playError()
+        },750)
+      } else {
+        setTyped(typedCompare)
+        setCorrect('green')
+      }
+      if(typedCompare === wordCompare) {
+        speak({ text : wordCompare , voice })
+        setTimeout(() => { 
+          playHooray()
+          setTimeout(() => {
+            chooseWord() 
+          },5000)
+        },2000)
+      }
+  }
+  setDisabled(false)
+  }
+
+  const toggleCap = () => {
+    if(cap) {
+      setCap(false)
+    }
+    if(!cap) {
+      setCap(true)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu fixed='top' inverted color='orange'>
+      <Container>
+        <Menu.Item as='a' header>
+          <Image size='mini' src={thumb} style={{ marginRight: '1.5em' }} />
+          JJs Typer
+        </Menu.Item>
+        <Menu.Item position='right'><Button onClick={() => toggleCap()}>{capLabel}</Button></Menu.Item>
+        {useDutchVoices.length > 0 && 
+        <Dropdown item simple text='Taal' closeOnChange>
+          <Dropdown.Menu>
+          {useDutchVoices.map((option, index) => (
+              <Dropdown.Item key={option.voiceURI} value={index} onClick={() => setVoiceIndex(index)}>
+                {`${option.name}`}
+              </Dropdown.Item>
+
+          ))}
+          </Dropdown.Menu>
+        </Dropdown>
+        }
+        {useDutchVoices.length === 0 &&
+            <Menu.Item>Installeer NL Spraak</Menu.Item>
+        }
+        <Menu.Item position='right'>About</Menu.Item>
+      </Container>
+    </Menu>
+
+
+    <Container text style={{ marginTop: '7em' }}>
+      <Header as='h1'>Typ alle letters uit het woord:</Header>
+      { word === '' &&
+        <Button onClick={() => chooseWord()} size='massive'>START</Button>
+      }
+      
+        {word !== '' &&
+          <div>
+            <Segment size='massive' inverted color={correct}>
+              {word}
+            </Segment>
+            <Input disabled={disabled} size='massive' value={typed} onChange={e => checkTyped(e.target.value)}/>
+           </div>
+        }
+        {
+          /*
+            <Button onClick={() => chooseWord()}>TESTING</Button> 
+          */
+        }
+        
+     
+    </Container>
     </div>
   );
 }
